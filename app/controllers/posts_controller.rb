fea7e190c22c810eb904f3b386e0952875189d53
simaonogueira101 <simao.nogueira66@gmail.com>
2018-07-05 00:59:55 +0100

    Adds username to post model.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -51,6 +51,6 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit(:title, :text)
+      params.require(:post).permit(:title, :text, :username)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180704131100) do
+ActiveRecord::Schema.define(version: 20180704234312) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,11 +30,18 @@ ActiveRecord::Schema.define(version: 20180704131100) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "controllers", force: :cascade do |t|
+    t.string "Search"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
   end
 
 end
